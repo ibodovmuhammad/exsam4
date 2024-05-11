7754600c6f0383abc699b8b61f1ddc3d9129b030
ibodovmuhammad <167885887+ibodovmuhammad@users.noreply.github.com>
2024-05-11 21:23:05 +0500

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,10 +1,4 @@
-def povv(a,b):
-    cum = 1
-    i=1
-    while i <=b:
-        cum = cum * b
-    print(cum)
-
-a= int (input())
-b= int (input())
-povv(a,b)
+def raj(n,m):
+    return n**m
+x,n=int(input()),int(input())
+print(raj(x,n))
